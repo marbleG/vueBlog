8755ccc282ec3005d16f1e5e29c0dc9dee42eb11
mabofu <1218892028@qq.com>
2022-09-03 14:08:34 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'java', link: '/guide/' },
+      { text: '百度一下', link: 'https://www.baidu.com' },
+    ]
+  }
+}
